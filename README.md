f974dcc7c296f6e4211510bcd168d3f994dc18d2
Anders Austad <anders.austad@novanet.no>
2018-10-13 18:34:30 +0200

    Add basic visualization of human coverage

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Simple .NET Core console application for collecting statistics from a set of rem
 ## Features
 
 - [x] Most recent commit for each repo (repo staleness)
+- [x] Number of commits per contributor
 - [ ] Number of unique contributors last X months (human coverage)
 - [ ] Any suggestions?
 
@@ -43,7 +44,7 @@ Example `appsettings.json` content:
 
 ## Status
 
-Incomplete proof-of-concept using [libgit2](https://github.com/libgit2/libgit2sharp). Currently clones/fetches repos, loops through commits and collects some key stats. No file output/persistence yet, only logs to console.
+Incomplete proof-of-concept using [libgit2](https://github.com/libgit2/libgit2sharp). Currently clones/fetches repos, loops through commits and collects some key stats. Simple visualization of staleness and coverage using chart.js.
 
 ### Todos
 
@@ -51,6 +52,7 @@ Incomplete proof-of-concept using [libgit2](https://github.com/libgit2/libgit2sh
 - [x] Clone/fetch remote repos
 - [x] Gather basic statistics from repos
 - [ ] Gather advanced statistics from repos
-- [ ] Use configured email aliases to find unique contributors (some contribute using multiple emails)
-- [ ] Persist key statistics to json file
-- [ ] Create static html page visualizing the result
+- [x] Use configured email aliases to find unique contributors (some contribute using multiple emails)
+- [x] Persist key statistics to json file
+- [x] Create basic static html page visualizing the result
+- [ ] Create more advanced html page visualizing the result using chart.js
--- src/RepoStats/Web/script.js
+++ src/RepoStats/Web/script.js
@@ -6,10 +6,11 @@
             return response.json();
         })
         .then(json => {
-            renderGraph(json);
+            renderStalenessChart(json);
+            renderAuthorCharts(json);
         });
 
-    var renderGraph = (json) => {
+    var renderStalenessChart = (json) => {
         var labels = [], data = [], colors = []
         json.repositories.forEach(function (repo, index) {
             labels.push(repo.name);
@@ -17,14 +18,13 @@
             colors.push(palette[index % palette.length]);
         });
 
-        var ctx = document.getElementById('chart').getContext('2d');
+        var ctx = document.getElementById('staleness-chart').getContext('2d');
         new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
                     backgroundColor: colors,
-                    borderColor: 'rgb(255, 255, 255)',
                     data: data,
                 }]
             },
@@ -36,9 +36,49 @@
         });
     }
 
+    var renderAuthorCharts = (json) => {
+        var containerEl = document.getElementById('author-charts-container');
+
+        json.repositories.forEach((repo, repoIndex) => {
+            var labels = [], data = [], colors = [];
+
+            var authorChartHeader = document.createElement("h3");
+            authorChartHeader.innerHTML = repo.name;
+            containerEl.appendChild(authorChartHeader);
+
+            var chartId = 'authorChart' + repoIndex;
+            var canvasEl = document.createElement("canvas");
+            canvasEl.setAttribute("id", chartId);
+            containerEl.appendChild(canvasEl);
+
+            repo.contributorCommitCounts.forEach((author, authorIndex) => {
+                labels.push(author.key);
+                data.push(author.value);
+                colors.push(piePalette[authorIndex % piePalette.length]);
+            });
+            var ctx = document.getElementById(chartId).getContext('2d');
+            new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        backgroundColor: colors,
+                        data: data,
+                    }]
+                },
+                options: {
+                    legend: {
+                        position: 'left'
+                    }
+                }
+            });
+        });
+    }
+
     var daysSince = (date) => {
         return moment().diff(moment(date), "days")
     }
 
     var palette = ["#ffb6b9", "#fae3d9", "#bbded6", "#61c0bf", "#aaaaaa"];
+    var piePalette = ["#bbded6", "#61c0bf", "#aaaaaa", "#ffb6b9", "#fae3d9"]
 })();
